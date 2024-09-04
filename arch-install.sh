a2af3a7a7c2292849312891962b862ae866f2478
JunaidQrysh <junaidqrysh@gmail.com>
2024-09-04 15:47:39 +0530

    Lots of improvement

--- arch-install.sh
+++ arch-install.sh
@@ -1,14 +1,59 @@
 #!/usr/bin/bash
 
+error_message() {
+    echo "$1" >&2
+}
+
+info_message() {
+    echo "$1"
+}
+
+list_timezones() {
+    echo "Here is the list of available timezones:"
+    echo "Use the arrow keys to scroll, and press 'q' to quit the pager."
+    find /usr/share/zoneinfo -type f | sed 's|/usr/share/zoneinfo/||' | sort | less
+}
+
+select_timezone() {
+    local timezones
+    local choice
+
+    timezones=$(mktemp)
+    find /usr/share/zoneinfo -type f | sed 's|/usr/share/zoneinfo/||' | sort > "$timezones"
+
+    echo "You will now see a list of available timezones."
+    echo "Use the arrow keys to scroll through the list, and press 'q' to exit the pager."
+    echo "Please make a note of the timezone you want to select."
+    echo
+    read -p "Press Enter to continue and view the list of timezones..."
+
+    list_timezones
+
+    echo
+    while true; do
+        read -p "Enter the timezone from the list (e.g., Europe/Paris): " TIMEZONE
+
+        if grep -q "^$TIMEZONE$" "$timezones"; then
+            info_message "Selected timezone: $TIMEZONE"
+            break
+        else
+            error_message "Error: Invalid selection. Please enter a valid timezone from the list."
+        fi
+    done
+
+    rm "$timezones"
+}
+
 function install {
+ echo "$host" > /mnt/etc/hostname
  cd /
  cp -rn "$dir"/Source/Linux/. /mnt/
  pacstrap -K /mnt base base-devel linux linux-firmware sof-firmware "$processor" "$btrfs_pkg" efibootmgr sudo neovim git networkmanager greetd thermald fish alsa-utils || { echo "Installation failed, Run the script again"; exit 1; }
  echo "KEYMAP=$(localectl status | grep 'VC Keymap' | awk '{print $3}')" > /mnt/etc/vconsole.conf
- mkdir -p /mnt/home/"$user"/Clone
- git clone "$dir" /mnt/home/"$user"/Clone
+ 
  arch-chroot /mnt bash -c '
- grub-install --efi-directory=/boot/efi --bootloader-id=Arch
+ grub-install --removable --efi-directory=/boot/efi --bootloader-id=Arch
+ grubu
  echo "Enter root account password:"
  while [ true ]
  do
@@ -29,7 +74,17 @@ if [ -d "/sys/class/power_supply" ]; then
 else
 	pacman -Runs thermald
 fi
- 
+ mkdir -p /mnt/home/"$user"/Clone
+ git clone "$dir" /mnt/home/"$user"/Clone
+select_timezone
+
+echo "Setting timezone to $TIMEZONE..."
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+
+info_message "Timezone set to $TIMEZONE."
+
+locale-gen
+
  sed -i "s/\"\"/\""$user"\"/" /etc/greetd/config.toml
  chsh -s /usr/bin/fish
  chsh -s /usr/bin/fish "$user"
@@ -42,6 +97,125 @@ dir=$(pwd)
 if [ -f "$dir"/ran.sh ]; then
 source "$dir"/ran.sh
 else
+
+
+
+list_disks() {
+    echo
+    echo "Available disks:"
+    lsblk -d -o NAME,SIZE,TYPE | grep "^sd\|^nvme\|^vd" | awk '{print "/dev/" $1 " " $2}'
+    echo
+}
+
+select_disk() {
+    local disks=()
+    local choice
+    local i=1
+    
+    mapfile -t disks < <(lsblk -d -o NAME,SIZE,TYPE | grep "^sd\|^nvme\|^vd" | awk '{print "/dev/" $1 " " $2}')
+    
+    while true; do
+        list_disks
+        
+        for i in "${!disks[@]}"; do
+            echo "$((i + 1))) ${disks[$i]}"
+        done
+
+        echo
+        read -p "Select a disk by number (1-${#disks[@]}): " choice
+        
+        if [[ "$choice" -ge 1 && "$choice" -le ${#disks[@]} ]]; then
+            DISK=${disks[$((choice - 1))]}
+            info_message "Selected disk: $DISK"
+            break
+        else
+            error_message "Error: Invalid selection. Please enter a number between 1 and ${#disks[@]}."
+        fi
+    done
+}
+
+get_swap_size() {
+    while true; do
+        echo
+        read -p "Enter the size of the swap partition (e.g., 2G for 2 GB): " SWAP_SIZE
+        
+        if [[ "$SWAP_SIZE" =~ ^[0-9]+[MGK]?$ ]]; then
+            info_message "Swap size: $SWAP_SIZE"
+            break
+        else
+            error_message "Error: Invalid size format. Please enter a size like '2G' or '512M'."
+        fi
+    done
+}
+
+select_disk
+
+get_swap_size
+
+while true; do
+    echo
+    read -p "Are you sure you want to partition $DISK? This operation will modify the disk. (yes/no): " CONFIRM
+    
+    if [[ "$CONFIRM" == "yes" ]]; then
+        break
+    elif [[ "$CONFIRM" == "no" ]]; then
+        info_message "Operation cancelled."
+        exit 0
+    else
+        error_message "Please enter 'yes' or 'no'."
+    fi
+done
+
+DISK_SIZE_SECTORS=$(gdisk -l "$DISK" | grep "Disk size" | awk '{print $4}')
+EFI_SIZE_SECTORS=$((100 * 1024 * 2))  # 100 MB in sectors (assuming 512 bytes per sector)
+
+SWAP_SIZE_SECTORS=$(echo "$SWAP_SIZE" | awk '/G/ {print $1 * 2048000} /M/ {print $1 * 409600} /K/ {print $1 * 409.6} /[0-9]/ {print $1 * 2048000}')
+
+LINUX_SIZE_SECTORS=$((REMAINING_SIZE_SECTORS - SWAP_SIZE_SECTORS))
+
+{
+    echo
+    echo "Creating partitions on $DISK..."
+    echo "o"         
+    echo "n"        
+    echo "1"         
+    echo ""          
+    echo "+100M"    
+    echo "ef00"     
+    
+    echo "n"        
+    echo "2"         
+    echo ""          
+    echo "+${SWAP_SIZE}" 
+    echo "8200"      
+    
+    echo "n"         
+    echo "3"         
+    echo ""          
+    echo ""          
+    echo "8300"      
+    
+    echo "w"        
+} | gdisk "$DISK" || error_message "Failed to partition the disk."
+
+info_message "Partitioning completed successfully."
+
+sleep 2
+
+if [[ "$DISK" =~ ^/dev/nvme ]]; then
+    efi="${DISK}p1"
+    swap="${DISK}p2"
+    device="${DISK}p3"
+else
+    efi="${DISK}1"
+    swap="${DISK}2"
+    device="${DISK}3"
+fi
+
+mkfs.fat -f -F32 "$efi"
+mkfs.btrfs -f "$device"
+mkswap -f "$swap"
+
 echo "Select Processor:"
 select yn in Intel Amd; do
     case "$yn" in
@@ -53,13 +227,11 @@ select yn in Intel Amd; do
 		;;
     esac
 done
-read -p "Enter install device path(/dev/xxx): " device
-read -p "Enter efi device path(/dev/xxx): " efi
-read -p "Enter swap device path(/dev/xxx): " swap
 read -p "Enter the user you would like to create: " user
 export user="$user"
+read -p "Enter the hostname: " host
 type=$(blkid -o value -s TYPE "$device")
-echo -e "#!/usr/bin/bash\nprocessor="$processor"\ndevice="$device"\nefi="$efi"\nswap="$swap"\nexport user="$user"\ntype="$type"" > "$dir"/ran.sh
+echo -e "#!/usr/bin/bash\nprocessor="$processor"\ndevice="$device"\nefi="$efi"\nswap="$swap"\nexport user="$user"\ntype="$type"\nhost=$host" > "$dir"/ran.sh
 fi
 
 if [ "$type" != "btrfs" ]; then
