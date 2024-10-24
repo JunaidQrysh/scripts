c507d5b5cc84679c7c24ad137a36be30351e41e4
JunaidQrysh <junaidqrysh@gmail.com>
2024-10-25 01:31:12 +0530

    Add flutter-setup

---
+++ flutter-setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+cd ~/Clone
+git clone https://github.com/flutter/flutter.git
+paru -S android-sdk android-sdk-build-tools android-sdk-cmdline-tools-latest android-platform android-sdk-platform-tools jdk21-openjdk
+sudo cp -R /opt/android-sdk ~/Clone
+sudo chown -R $(whoami):$(whoami) ~/Clone/android-sdk
+flutter --disable-analytics
+yes | flutter doctor --android-licenses
+echo "Log out and log in again to enable flutter"
